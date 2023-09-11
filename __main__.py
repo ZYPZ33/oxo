df11eae8bd7c624c44548524f0a5f2696d51636e
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-11 18:36:28 +0100

    1. linked __main__.py -> oxo.py 2. modified .gitignore 3. Fixed oxo.py problems

--- __main__.py
+++ __main__.py
@@ -1,18 +1,1 @@
-#!/usr/bin/env python3
-from sys import argv
-from os import system
-
-filename="oxo.py"
-directory="/usr/local/bin"
-
-def runGame():
-    with open(filename) as code:
-        exec(code.read())
-
-if len(argv) > 1:
-    if "install" in argv:
-        system("install",filename,directory)
-    if "run" in argv:
-        runGame()
-else:
-    runGame()
+oxo.py
