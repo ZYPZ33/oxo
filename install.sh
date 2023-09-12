1061c703f076555c3146dceaf1e76abbd9052893
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-12 11:21:34 +0100

    Improved install.sh verbosity

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 executable_name="oxo"
 install_directory="/usr/local/bin"
-install oxo.py ${install_directory}/${executable_name}
+install oxo.py ${install_directory}/${executable_name}&&echo "${executable_name} installed at ${install_directory}"
