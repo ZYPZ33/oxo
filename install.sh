cb12d7c2891284bc12ac695058969b55e2645ca8
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-13 08:44:39 +0100

    Added shebang to install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 executable_name="oxo"
 install_directory="/usr/local/bin"
 install oxo.py ${install_directory}/${executable_name}&&echo "${executable_name} installed at ${install_directory}"
