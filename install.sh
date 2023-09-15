da666ce8ad2113953d8e08faa9b34907d7990c4c
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-15 08:49:14 +0100

    Added check for if program is already installed

--- install.sh
+++ install.sh
@@ -4,5 +4,11 @@ install_directory="/usr/local/bin"
 if [ -n "$1" ]
 then executable_name="$1"
 fi
-install oxo.py ${install_directory}/${executable_name}&&\
+test -e ${install_directory}/${executable_name}&& \
+read -p "Do you want to replace the already installed version? [Y/n]" input
+
+#if [ "${input}" = "y" ||  "${input}" = "Y" ]
+if [ "${input}" = "y" ] || [ "${input}" = "Y" ]
+  then install oxo.py ${install_directory}/${executable_name}&&\
 echo "${executable_name} installed at ${install_directory}"
+fi
