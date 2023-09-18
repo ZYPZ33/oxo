4eabe57ec724fdb40066b21b2ea0ecb33c3ffab4
ZYPZ <72951371+ZYPZ33@users.noreply.github.com>
2023-09-18 09:46:45 +0100

    Added features to test.py

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-from guizero import App, Text, PushButton
+from guizero import App, Text, PushButton, Picture, TextBox
 
 titlename = "Hello Qt!"
 app = App(title=titlename, width=200, height=200)
 board = [["_" for row in range(3)] for column in range(3)]
 
 o1 = Text(app, text="\nHello,\n Openbox!")
-i1 = PushButton(app, text="Hello!", command=exit)
+
+o2 = Picture(app)
+o2.image = "/home/user/image.jpg"
+
+i1 = TextBox(app)
+
+i2 = PushButton(app, text="Hello!", command=exit)
+
+
 app.display()
