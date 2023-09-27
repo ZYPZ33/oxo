b00f5de4760287ccfa14e0c75351d3d1ec3f4e65
June Young <june_@tutanota.com>
2023-09-27 13:33:05 +0100

    Added functionality to oxo-gui.py

--- oxo-gui.py
+++ oxo-gui.py
@@ -3,6 +3,16 @@ from sys import argv, exit
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGroupBox, QSlider, QLCDNumber
 
+if "-h" in argv or "--help" in argv:
+    print(
+        "Usage: oxo OPTION...\n\
+    --help   -h\tPrint this help message\n\
+    --size   -s\tSet the size of the game board (must be greater than 3)\n\
+    --level  -l\tSet the AI level (1-3)\n\
+    --repeat -r\tMake game loop"
+          )
+    exit()
+
 size = 3
 board = [["_" for row in range(size)] for column in range(size)]
 class MainWindow(QWidget):
@@ -27,9 +37,9 @@ class MainWindow(QWidget):
             for column in range(len(board)):
                 self.button[row][column] = QPushButton("X", self)
                 self.button[row][column].setGeometry(40*column+80,40*row+20,40,40)
-                self.button[row][column].clicked.connect(self.on_button_click)
-    def on_button_click(self):
-        print(self.button[][])
+                self.button[row][column].clicked.connect(lambda: self.on_button_click(row,column))
+    def on_button_click(self,x,y):
+        print(self.button[x][y].text())
 
 if __name__ == "__main__":
     app = QApplication(argv)
